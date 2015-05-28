5424b242b7d70df0e3bb9c81370cbb1752f23676
JuneHyeon Bae <devunt@gmail.com>
2015-05-28 09:01:13 +0200

    Fix styles

--- ika/conf.py
+++ ika/conf.py
@@ -3,6 +3,7 @@ import json
 
 from ika.utils import edict
 
+
 basedir = os.path.dirname(sys.argv[0])
 filepath = os.path.join(basedir, 'config.json')
 
--- ika/logger.py
+++ ika/logger.py
@@ -1,5 +1,6 @@
 import logging
 
+
 logging.basicConfig(format='[%(asctime)s] {%(levelname)s} %(message)s')
 logger = logging.getLogger('ika')
 logger.setLevel(logging.DEBUG)
