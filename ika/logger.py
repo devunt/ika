b06e7e55f906503b34f6d218d70cd469500ec7f8
JuneHyeon Bae <devunt@gmail.com>
2015-05-29 00:37:48 +0200

    Add logging level configuration

--- ika/logger.py
+++ ika/logger.py
@@ -1,6 +1,8 @@
 import logging
 
+from ika.conf import settings
+
 
 logging.basicConfig(format='[%(asctime)s] {%(levelname)s} %(message)s')
 logger = logging.getLogger('ika')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(getattr(logging, settings.logging))
