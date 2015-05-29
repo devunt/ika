8a06ae35270f3832eb5052f47bbb64ebe30c27c6
JuneHyeon Bae <devunt@gmail.com>
2015-05-30 01:58:32 +0200

    Add description of each services

--- ika/services/legacy/nickserv/__init__.py
+++ ika/services/legacy/nickserv/__init__.py
@@ -6,3 +6,7 @@ class NickServ(Service):
     aliases = [
         'NickServ',
     ]
+    description = [
+        '과거에 닉네임 서비스 봇이었습니다.',
+        '하위호환을 위해 자주 쓰였던 닉섭 명령들을 새 통합 서비스봇으로 넘겨주는 역할을 합니다.',
+    ]
--- ika/services/ozinger/__init__.py
+++ ika/services/ozinger/__init__.py
@@ -3,4 +3,8 @@ from ika.classes import Service
 
 
 class Ozinger(Service):
-    name = 'O'
+    name = 'ika'
+    description = [
+        '통합 서비스봇입니다.',
+        '채널, 계정(닉네임) 관리 등을 할 수 있습니다.',
+    ]
