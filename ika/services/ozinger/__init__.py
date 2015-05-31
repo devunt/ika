517e8e058f88ddc7b4e1418cfa22a1ea1f2165fa
JuneHyeon Bae <devunt@gmail.com>
2015-05-31 23:25:48 +0200

    Change services.ozinger name

--- ika/services/ozinger/__init__.py
+++ ika/services/ozinger/__init__.py
@@ -3,7 +3,10 @@ from ika.classes import Service
 
 
 class Ozinger(Service):
-    name = 'ika'
+    name = '이카무스메'
+    aliases = (
+        'ika',
+    )
     description = (
         '통합 서비스봇입니다.',
         '채널, 계정(닉네임) 관리 등을 할 수 있습니다.',
