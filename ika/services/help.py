1b1d5865c78c0aa0b5cd45d3690ba882232616fd
JuneHyeon Bae <devunt@gmail.com>
2015-06-01 22:43:58 +0200

    Fix services.help

--- ika/services/help.py
+++ ika/services/help.py
@@ -9,21 +9,21 @@ class Help(Command):
         '도움',
     )
     syntax = '[명령어 이름]'
+    regex = r'(?P<command>\S+)?'
     description = (
         '특정 서비스봇이나 명령에 대한 도움말을 보여줍니다.',
     )
 
     @asyncio.coroutine
-    def execute(self, uid, *params):
-        if len(params) == 0:
-            target = self.service
-        else:
-            command = params[0]
+    def execute(self, uid, command):
+        if command:
             if command in self.service.commands:
                 target = self.service.commands[command]
             else:
                 self.service.msg(uid, '해당 명령이 존재하지 않아 도움말을 찾을 수 없습니다. \x02/msg {} 도움말\x02 을 입력해보세요.', self.service.name)
                 return
+        else:
+            target = self.service
         self.service.msg(uid, '==== \x02{}\x02 도움말 ====', target.name)
         if isinstance(target, Command):
             self.service.msg(uid, '사용법: \x02/msg {} {} {}\x02', self.service.name, target.name, target.syntax)
