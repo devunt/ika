db47f01a3c64773317b4607270f57de958cfa03f
JuneHyeon Bae <devunt@gmail.com>
2015-06-05 01:57:49 +0200

    Slightly modify message in services.ozinger.events.check_registered

--- ika/services/ozinger/events/check_registered.py
+++ ika/services/ozinger/events/check_registered.py
@@ -10,7 +10,7 @@ class CheckRegistered(Listener):
         session = Session()
         nick = session.query(Nick).filter_by(name=name).first()
         if nick and (nick.account or nick.account_alias):
-            self.service.msg(uid, '이 닉네임은 이미 오징어 IRC 네트워크에 등록되어 있는 닉네임입니다. 계정의 주인이시라면 \x02/msg {} 로그인\x02 을, 주인이 아니시라면 지금 바로 닉네임을 다른 것으로 바꿔 주세요.', self.service.name)
+            self.service.msg(uid, '이 닉네임은 이미 오징어 IRC 네트워크에 등록되어 있는 닉네임입니다. 계정의 주인이시라면 \x02/msg {} 로그인\x02 을, 주인이 아니시라면 지금 닉네임을 다른 것으로 바꿔 주세요.', self.service.name)
 
     @asyncio.coroutine
     def NICK(self, user, *params):
