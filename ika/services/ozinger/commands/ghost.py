950d84a39b24b48cc8cb3ce598cea4cc4632af26
JuneHyeon Bae <devunt@gmail.com>
2015-06-05 01:56:51 +0200

    Add services.ozinger.commands.ghost

---
+++ ika/services/ozinger/commands/ghost.py
@@ -0,0 +1,32 @@
+import asyncio
+
+from ika.classes import Command
+from ika.enums import Permission
+from ika.database import Nick, Account, Session
+
+
+class Ghost(Command):
+    name = '고스트'
+    aliases = (
+    )
+    syntax = '<닉네임>'
+    regex = r'(?P<nick>\S+)'
+    permission = Permission.LOGIN_REQUIRED
+    description = (
+        '오징어 IRC 네트워크에 등록된 계정에 연결된 닉네임을 사용중인 사용자의 연결을 강제로 끊습니다.',
+        ' ',
+        '오징어 IRC 네트워크에 등록되어 있는 계정에 연결된 닉네임을 사용중인 사용자의 연결을 강제로 종료합니다.',
+        '기존에 연결되어 있는 본인의 접속 세션을 끊을 수도 있고,',
+        '본인이 등록한 닉네임을 허락 없이 쓰고 있는 사용자의 연결을 강제로 종료시킬 수도 있습니다.',
+    )
+
+    @asyncio.coroutine
+    def execute(self, user, nick):
+        session = Session()
+        account = session.query(Account).filter(Nick.name==nick).first()
+        if account is not user.account:
+            self.service.msg(user, '해당 닉네임이 \x02{}\x02 계정에 속해 있지 않습니다.', user.account.name.name)
+            return
+        for target in self.service.server.users.values():
+            if target.nick == nick:
+                self.service.writesvsuserline('KILL {} :{}!{}@{} 에 의한 고스트', target.uid, user.nick, user.ident, user.dhost)
