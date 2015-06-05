c8f1431ae539a8e6317b72dc4aade3f72f3c2d0b
JuneHyeon Bae <devunt@gmail.com>
2015-06-05 15:38:42 +0200

    Edit services.ozinger.name and welcome message

--- ika/services/ozinger/__init__.py
+++ ika/services/ozinger/__init__.py
@@ -2,11 +2,13 @@ from ika.classes import Service
 
 
 class Ozinger(Service):
-    name = '이카무스메'
+    name = '오징어'
     aliases = (
-        'ika',
     )
     description = (
         '통합 서비스봇입니다.',
         '채널, 계정(닉네임) 관리 등을 할 수 있습니다.',
+        ' ',
+        '현재 시범적으로 기존 닉섭(오징오징어)을 대체하여 운영중입니다.',
+        '소스코드: https://github.com/devunt/ika',
     )
--- ika/services/ozinger/events/welcome.py
+++ ika/services/ozinger/events/welcome.py
@@ -7,3 +7,5 @@ class Welcome(Listener):
     @asyncio.coroutine
     def UID(self, server, *params):
         self.service.msg(params[0], '오징어 IRC 네트워크에 오신 것을 환영합니다! 서비스봇 사용을 위해서는 \x02/msg {0} 도움말\x02 을 입력해보세요.', self.service.name)
+        self.service.msg(params[0], '오징어의 새 서비스봇인 ika는 현재 시범적으로 \x02오징오징어\x02 를 대체하고 있습니다. ㅇㅈㅇ는 앞으로 대체될 예정에 있습니다.')
+        self.service.msg(params[0], 'ika는 오픈소스로 개발되고 있으며 소스코드는 https://github.com/devunt/ika 에서 보실 수 있습니다. 많은 기여와 이슈 제보 부탁드립니다. 감사합니다.')
