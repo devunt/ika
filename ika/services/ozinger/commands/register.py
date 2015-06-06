15bbc33fed6a92666b0ec537d9f1c7b470c9499a
JuneHyeon Bae <devunt@gmail.com>
2015-06-06 18:27:50 +0200

    Check nickname integrity on services.ozinger.commands.register

--- ika/services/ozinger/commands/register.py
+++ ika/services/ozinger/commands/register.py
@@ -26,6 +26,10 @@ class Register(Command):
     def execute(self, user, email, password):
         session = Session()
 
+        if Nick.find_by_name(user.nick):
+            self.service.msg(user, '해당 닉네임 \x02{}\x02 은 이미 오징어 IRC 네트워크에 등록되어 있습니다.', user.nick)
+            break
+
         nick = Nick()
         nick.name = user.nick
         nick.last_use = datetime.now()
