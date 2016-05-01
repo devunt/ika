162e8a8f5058403250e1ab32245104b23190c743
Junehyeon Bae <devunt@gmail.com>
2016-05-01 00:24:58 -0700

    Add services.ozinger.events.guard

---
+++ ika/services/ozinger/events/guard.py
@@ -0,0 +1,42 @@
+import asyncio
+
+from ika.classes import Listener
+from ika.database import Channel, Session
+
+
+class Guard(Listener):
+    @asyncio.coroutine
+    def ENDBURST(self, server, linked_once):
+        if linked_once:
+            return
+        session = Session()
+        channels = session.query(Channel).all()
+        for channel in channels:
+            real_channel = self.service.server.channels.get(channel.name.lower())
+            if real_channel:
+                self.service.join_channel(real_channel)
+
+    @asyncio.coroutine
+    def FJOIN(self, server, *params):
+        channel = Channel.find_by_name(params[0])
+        if not channel:
+            return
+        real_channel = self.service.server.channels.get(channel.name.lower())
+        if not real_channel:
+            return
+        if self.service.uid not in real_channel.users.keys():
+            self.service.join_channel(real_channel)
+
+    @asyncio.coroutine
+    def PART(self, user, channel, *params):
+        if channel not in self.service.joined_channels:
+            return
+        real_channel = self.service.server.channels.get(channel)
+        if not real_channel:
+            self.service.part_channel(channel, 'Never left without saying goodbye')
+
+    @asyncio.coroutine
+    def QUIT(self, user, *params):
+        for channel in self.service.joined_channels:
+            if channel not in self.service.server.channels.keys():
+                self.service.part_channel(channel, 'Never left without saying goodbye')
