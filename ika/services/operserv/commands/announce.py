4c298e94c467decb3678a7bb21659a2ef3660a9c
Junehyeon Bae <devunt@gmail.com>
2016-07-17 13:26:21 +0900

    공지사항 발송시 메시지 추가

--- ika/services/operserv/commands/announce.py
+++ ika/services/operserv/commands/announce.py
@@ -21,5 +21,8 @@ class Announce(Command):
 
     @asyncio.coroutine
     def execute(self, user, text):
+        channelcount = len(self.service.server.channels)
+        self.service.msg(user, '{}개 채널에 공지사항을 발송합니다.', channelcount)
         for channelname in self.service.server.channels.keys():
             self.service.writesvsuserline('PRIVMSG {} : \x02[공지]\x02 {}', channelname, text)
+        self.service.msg(user, '{}개 채널에 공지사항을 발송했습니다.', channelcount)
