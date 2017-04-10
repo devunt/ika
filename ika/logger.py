f32af906d66b94bc647c52540dd0fe1e4626cadb
Junehyeon Bae <devunt@gmail.com>
2017-04-10 14:55:58 +0900

    Import raven pacakges If only raven dsn is present in settings

--- ika/logger.py
+++ ika/logger.py
@@ -1,18 +1,19 @@
 import logging
-from raven import Client
-from raven.conf import setup_logging
-from raven.handlers.logging import SentryHandler
 
 from ika.conf import settings
 
 
 logging.basicConfig(format='[%(asctime)s] {%(levelname)s} %(message)s')
 
-if settings.raven_dsn is not None:
-    client = Client(dsn=settings.raven_dsn, auto_log_stacks=True)
+if settings.logging.sentry.dsn is not None:
+    from raven import Client
+    from raven.conf import setup_logging
+    from raven.handlers.logging import SentryHandler
+
+    client = Client(dsn=settings.logging.sentry.dsn, auto_log_stacks=True)
     handler = SentryHandler(client)
-    handler.setLevel(logging.WARNING)
+    handler.setLevel(getattr(logging, settings.logging.sentry.level))
     setup_logging(handler)
 
 logger = logging.getLogger('ika')
-logger.setLevel(getattr(logging, settings.logging))
+logger.setLevel(getattr(logging, settings.logging.console.level))
