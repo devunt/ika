88178eee3b0eba6dad766c90f69edd07a5baf02a
Junehyeon Bae <devunt@gmail.com>
2017-04-11 18:56:58 +0900

    Move announce command into ozinger service

--- ika/services/ozinger/commands/announce.py
+++ ika/services/ozinger/commands/announce.py
@@ -12,14 +12,13 @@ class Announce(Command):
     permission = Permission.OPERATOR
     description = (
         '모든 IRC 채널에 공지사항을 출력합니다.',
-        '^^ 명의의 PRIVMSG를 이용해 채널 라인으로 출력하므로 주의하십시오.',
+        'PRIVMSG를 이용해 채널 라인으로 출력하므로 주의하십시오.',
         '명령을 실행하는 순간 발송하므로 명령을 실행하기 전 다시 한번 공지사항의 내용을 확인해주세요.',
     )
 
     async def execute(self, user, text):
-        uid = self.server.services_instances['Ozinger'].uid
         count = len(self.server.channels)
         self.msg(user, f'{count}개 채널에 공지사항을 발송합니다.')
         for cname in self.server.channels.keys():
-            self.server.writeuserline(uid, f'PRIVMSG {cname} : \x02[공지]\x02 {text}')
+            self.writesvsuserline(f'PRIVMSG {cname} : \x02[공지]\x02 {text}')
         self.msg(user, f'{count}개 채널에 공지사항을 발송했습니다.')
