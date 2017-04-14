c2f98f13148560c9d3a56d5d02a6542177bcfb66
Junehyeon Bae <devunt@gmail.com>
2017-04-15 05:19:00 +0900

    Correct term usage

--- ika/services/ozinger/commands/unregister.py
+++ ika/services/ozinger/commands/unregister.py
@@ -16,7 +16,7 @@ class Unregister(Command):
         ' ',
         '이 명령을 사용할 시 오징어 IRC 네트워크에 등록되어 있는 계정의 등록을 해제하며,',
         '그 뒤로는 네트워크에서 제공하는 여러 편의 기능등을 이용하실 수 없습니다.',
-        '계정에 \x02F\02 (파운더) 권한이 있는 채널이 있을 경우 등록을 해제할 수 없습니다.',
+        '계정에 \x02F\02 (개설자) 권한이 있는 채널이 있을 경우 등록을 해제할 수 없습니다.',
         '실수로 명령을 실행하는 것을 방지하기 위해 명령 맨 뒤에 \x02YES\x02 를 붙여야 합니다.',
     )
 
@@ -28,7 +28,7 @@ class Unregister(Command):
 
         for flag in user.account.channel_flags.all():
             if Flags.FOUNDER in flag.flags:
-                self.err(user, f'\x02{flag.channel}\x02 채널에 \x02F\x02 (파운더) 권한이 있어 해당 계정 \x02{name}\x02 의 등록을 해제할 수 없습니다.')
+                self.err(user, f'\x02{flag.channel}\x02 채널에 \x02F\x02 (개설자) 권한이 있어 해당 계정 \x02{name}\x02 의 등록을 해제할 수 없습니다.')
 
         user.account.delete()
         self.writeserverline('METADATA', user.uid, 'accountname', '')
