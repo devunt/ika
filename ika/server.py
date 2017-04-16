5944c2c2039b31b7327764b37059ba53b1bafb13
Junehyeon Bae <devunt@gmail.com>
2017-04-17 04:28:46 +0900

    SQUIT message should be except_event

--- ika/server.py
+++ ika/server.py
@@ -157,5 +157,5 @@ class Server:
     def disconnect(self, reason=''):
         for instance in self.services.values():
             instance.unload_irc_bots(reason=reason)
-        self.writeserverline('SQUIT', self.link.name, reason)
+        self.writeserverline('SQUIT', self.link.name, reason, exempt_event=True)
         self.writeserverline(f'ERROR :Service disconnected ({reason})', exempt_event=True)
