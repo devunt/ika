a79f4d4f73f3d3391cb3cb79db234fcfc9aaf279
Junehyeon Bae <devunt@gmail.com>
2017-04-25 03:21:50 +0900

    Add channel data handling

--- ika/services/ozinger/events/channel_join.py
+++ ika/services/ozinger/events/channel_join.py
@@ -10,6 +10,19 @@ class ChannelJoin(Listener):
         irc_channel = self.server.channels[cname]
         for umode in modes_n_umodes[-1].split():
             uid = umode.split(',')[1]
+
             modestring = irc_channel.generate_synchronizing_modestring(uid)
             if modestring:
                 self.writesvsuserline('FMODE', cname, irc_channel.timestamp, modestring)
+
+            entrymsg = channel.data.get('entrymsg')
+            if entrymsg:
+                self.msg(uid, f'\x02[{channel.name}] {entrymsg}\x02')
+
+            url = channel.data.get('url')
+            if url:
+                self.msg(uid, f'\x02[{channel.name}] 채널 웹사이트: {url}\x02')
+
+            email = channel.data.get('email')
+            if email:
+                self.msg(uid, f'\x02[{channel.name}] 채널 이메일: {email}\x02')
